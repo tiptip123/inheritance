b910e0476dbe805880293a2c48a42b2a952ae0cd
tiptip123 <josephbaguio327@gmail.com>
2023-11-09 16:27:01 +0800

    fix some issues and added broke method in class RTX4090

--- src/gwapongait/RTX4090.java
+++ src/gwapongait/RTX4090.java
@@ -1,14 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gwapongait;
 
-/**
- *
- * @author Admin
- */
-public class RTX4090 {
-    
+public class RTX4090 extends VideoCard {
+
+    RTX4090(String videoCard) {
+        super(videoCard);
+
+    }
+
+    void broke() {
+        System.out.println(videoCard + " is broken");
+
+    }
+
 }
